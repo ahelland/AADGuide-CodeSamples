e959962800decde975e62fe6dbd20e00d8d0fca1
Andreas Helland <andreas@mobilitydojo.net>
2015-07-29 13:30:04 -0700

    Changed AuthString constant to login.microsoftonline.com

--- GraphTreeView/Constants.cs
+++ GraphTreeView/Constants.cs
@@ -4,8 +4,8 @@
     {
         // Note: update the string TenantId with your TenantId.
         // This can be retrieved from the login Federation Metadata end point:             
-        // https://login.windows.net/GraphDir1.onmicrosoft.com/FederationMetadata/2007-06/FederationMetadata.xml
-        //  Replace "GraphDir1.onMicrosoft.com" with any domain owned by your organization
+        // https://login.microsoftonline.com/contoso.onmicrosoft.com/FederationMetadata/2007-06/FederationMetadata.xml
+        //  Replace "contoso.onmicrosoft.com" with any domain owned by your organization
         // The returned value from the first xml node "EntityDescriptor", will have a STS URL
         // containing your TenantId e.g. "https://sts.windows.net/4fd2b2f2-ea27-4fe5-a8f3-7b1a7c975f34/" is returned for GraphDir1.onMicrosoft.com
 
@@ -13,7 +13,7 @@
         public const string TenantId = "guid";
         public const string ClientId = "get-from-Azure-Portal";
         public const string ClientSecret = "get-from-Azure-Portal";
-        public const string AuthString = "https://login.windows.net/" + TenantName;
+        public const string AuthString = "https://login.microsoftonline.com/" + TenantName;
         public const string ResourceUrl = "https://graph.windows.net";
     }
 }
