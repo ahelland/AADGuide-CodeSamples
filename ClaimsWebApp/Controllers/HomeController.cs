e2d07a92b31c406f3bad9a5b1da0eafd716a03d7
Andreas Helland <andreas@mobilitydojo.net>
2016-03-16 10:51:37 -0700

    Added ClaimsWebApp sample

---
+++ ClaimsWebApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace ClaimsWebApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Claim displayName = ClaimsPrincipal.Current.FindFirst(ClaimsPrincipal.Current.Identities.First().NameClaimType);
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+
+            var user = User as ClaimsPrincipal;
+            var identity = user.Identity as ClaimsIdentity;
+            return View();
+        } 
+              
+        public ActionResult AddClaim()
+        {
+            Claim displayName = ClaimsPrincipal.Current.FindFirst(ClaimsPrincipal.Current.Identities.First().NameClaimType);
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+
+            //ClaimType, Value, ValueType, Issuer
+            Claim localClaim = new Claim(ClaimTypes.Webpage, "http://localhost", ClaimValueTypes.String, "AADGuide");
+
+            ClaimsPrincipal.Current.Identities.First().AddClaim(localClaim);
+            return View("Index");
+        }
+
+        public ActionResult RemoveClaim()
+        {
+            Claim customClaim = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Webpage);
+
+            var user = User as ClaimsPrincipal;
+            var identity = user.Identity as ClaimsIdentity;
+
+            ClaimsPrincipal.Current.Identities.First().RemoveClaim(customClaim);
+            return View("Index");
+        }
+    }
+}
---
+++ ClaimsWebApp/Utils/ClaimsTransformation.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ClaimsWebApp.Utils
+{
+    public class ClaimsTransformationMiddleware
+    {       
+        readonly Func<IDictionary<string, object>, Task> _next;       
+
+        public ClaimsTransformationMiddleware(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+        
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            ClaimsPrincipal incomingPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            if (incomingPrincipal != null && incomingPrincipal.Identity.IsAuthenticated == true)
+            {
+                ClaimsIdentity claimsIdentity = incomingPrincipal.Identity as ClaimsIdentity;
+        
+                string tenantId = incomingPrincipal.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+                
+                //Replace with tenant id for xyz.onmicrosoft.net
+                string adminTenant = "1234-abcd-GUID";
+
+                if (tenantId == adminTenant)
+                {
+                    ((ClaimsIdentity)incomingPrincipal.Identity).AddClaim(new Claim(ClaimTypes.Role, "Admin", ClaimValueTypes.String, "AADGuide"));
+                }
+                
+                Thread.CurrentPrincipal = incomingPrincipal;        
+            }
+            await _next(env);
+        }        
+    }
+}
