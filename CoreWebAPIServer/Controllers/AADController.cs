2f618518191c0421a2c092e32e791102c5cdd991
Andreas Helland <andreas@mobilitydojo.net>
2016-07-04 11:51:54 -0700

    Added CoreWebAPIServer sample

---
+++ CoreWebAPIServer/Controllers/AADController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoreWebAPIServer.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AADController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "World" };
+        }
+
+    }
+}
