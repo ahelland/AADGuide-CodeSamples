be33fa96cc91efb68329e5c92c4325dc5d95bae8
Andreas Helland <andreas@mobilitydojo.net>
2016-12-12 13:39:13 -0800

    Added AboutMePasswordGrant sample

---
+++ AboutMePasswordGrant/AboutMeForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AboutMePasswordGrant
+{
+    partial class AboutMeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAADDomainName = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblResource = new System.Windows.Forms.Label();
+            this.lblClientId = new System.Windows.Forms.Label();
+            this.txtAADDomainName = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtResource = new System.Windows.Forms.TextBox();
+            this.txtClientId = new System.Windows.Forms.TextBox();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAADDomainName
+            // 
+            this.lblAADDomainName.AutoSize = true;
+            this.lblAADDomainName.Location = new System.Drawing.Point(10, 10);
+            this.lblAADDomainName.Name = "lblAADDomainName";
+            this.lblAADDomainName.Size = new System.Drawing.Size(102, 13);
+            this.lblAADDomainName.TabIndex = 0;
+            this.lblAADDomainName.Text = "AAD Domain Name:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(10, 135);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(42, 13);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "Output:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(10, 110);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(10, 85);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblResource
+            // 
+            this.lblResource.AutoSize = true;
+            this.lblResource.Location = new System.Drawing.Point(10, 60);
+            this.lblResource.Name = "lblResource";
+            this.lblResource.Size = new System.Drawing.Size(56, 13);
+            this.lblResource.TabIndex = 4;
+            this.lblResource.Text = "Resource:";
+            // 
+            // lblClientId
+            // 
+            this.lblClientId.AutoSize = true;
+            this.lblClientId.Location = new System.Drawing.Point(10, 35);
+            this.lblClientId.Name = "lblClientId";
+            this.lblClientId.Size = new System.Drawing.Size(45, 13);
+            this.lblClientId.TabIndex = 5;
+            this.lblClientId.Text = "ClientId:";
+            // 
+            // txtAADDomainName
+            // 
+            this.txtAADDomainName.Location = new System.Drawing.Point(130, 10);
+            this.txtAADDomainName.Name = "txtAADDomainName";
+            this.txtAADDomainName.Size = new System.Drawing.Size(250, 20);
+            this.txtAADDomainName.TabIndex = 0;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(130, 85);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(250, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(130, 110);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(250, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(130, 135);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(250, 60);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // txtResource
+            // 
+            this.txtResource.Location = new System.Drawing.Point(130, 60);
+            this.txtResource.Name = "txtResource";
+            this.txtResource.Size = new System.Drawing.Size(250, 20);
+            this.txtResource.TabIndex = 2;
+            this.txtResource.Text = "https://graph.microsoft.com";
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Location = new System.Drawing.Point(130, 35);
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(250, 20);
+            this.txtClientId.TabIndex = 1;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(10, 155);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(100, 40);
+            this.btnAction.TabIndex = 6;
+            this.btnAction.Text = "Get Token &&\r\nDo Lookup";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // AboutMeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 212);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.txtClientId);
+            this.Controls.Add(this.txtResource);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtAADDomainName);
+            this.Controls.Add(this.lblClientId);
+            this.Controls.Add(this.lblResource);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblAADDomainName);
+            this.Name = "AboutMeForm";
+            this.Text = "About Me - Password Grant Flow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAADDomainName;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblResource;
+        private System.Windows.Forms.Label lblClientId;
+        private System.Windows.Forms.TextBox txtAADDomainName;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtResource;
+        private System.Windows.Forms.TextBox txtClientId;
+        private System.Windows.Forms.Button btnAction;
+    }
+}
+
---
+++ AboutMePasswordGrant/AboutMeForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using static System.Environment;
+
+namespace AboutMePasswordGrant
+{
+    public partial class AboutMeForm : Form
+    {
+        public AboutMeForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAction_Click(object sender, EventArgs e)
+        {
+            var domain = txtAADDomainName.Text;
+            var user = txtUsername.Text;
+            var pw = txtPassword.Text;
+            var clientId = txtClientId.Text;
+            var resource = txtResource.Text;
+
+            HttpClient client = new HttpClient();
+
+            string requestUrl = $"https://login.microsoftonline.com/{domain}/oauth2/token";
+
+            string request_content = $"grant_type=password&resource={resource}&client_id={clientId}&username={user}&password={pw}&scope=openid";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+            try
+            {
+                request.Content = new StringContent(request_content, Encoding.UTF8, "application/x-www-form-urlencoded");
+            }
+            catch (Exception x)
+            {
+                var msg = x.Message;
+                txtOutput.Text = $"Something went wrong: {msg}";
+            }
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+            GenericToken token = JsonConvert.DeserializeObject<GenericToken>(responseString);
+            var at = token.access_token;
+
+            var me = await GetUserInfo(at);
+
+            txtOutput.Text = $"Display Name:{me.displayName}{NewLine}Upn:{me.userPrincipalName}{NewLine}Preferred Language:{me.preferredLanguage}";
+
+        }
+
+        private static async Task<AADUser> GetUserInfo(string token)
+        {
+            string graphRequest = $"https://graph.microsoft.com/v1.0/me/";
+            var authHeader = "Bearer " + token;
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", authHeader);
+            var response = await client.GetAsync(new Uri(graphRequest));
+
+            string content = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<AADUser>(content);
+            return user;
+        }
+
+        internal class GenericToken
+        {
+            public string token_type { get; set; }
+            public string scope { get; set; }
+            public string resource { get; set; }
+            public string access_token { get; set; }
+            public string refresh_token { get; set; }
+            public string id_token { get; set; }
+            public string expires_in { get; set; }
+        }
+
+        //A selection of attributes to include in deserialization - there are more available
+        internal class AADUser
+        {
+            public string displayName { get; set; }
+            public string givenName { get; set; }
+            public string surname { get; set; }
+            public string mail { get; set; }
+            public string preferredLanguage { get; set; }
+            public string userPrincipalName { get; set; }
+            public string mobilePhone { get; set; }
+        }
+
+    }
+}
